437c65e24e9adc7f0f6d1a5ca69b5b3cde75fe14
Galenwan <ferdinandvalentino1@gmail.com>
2025-04-15 23:22:44 +0700

    Third Commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+function replaceName() {
+    let name = prompt("Hi~~, what's your name?", "");
+    document.getElementById("name").innerHTML = name
+}
+
+replaceName();
+
+function validationForm() {
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+
+    if (  name == "" || birthDate == "" || gender == "" || messages == "" ) {
+        alert("There must not be anything unfilled");
+        return false;
+    }
+
+    setSenderUI(name, birthDate, gender, messages);
+    
+    return false;
+}
+
+function setSenderUI(name, birthDate, gender, messages) {
+    document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-birth-date").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-messages").innerHTML = messages;
+}
+
+var slideIndex = 1
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var imgList = document.getElementsByClassName("img-slideshow");
+    if (n> imgList.length) slideIndex = 1;
+    else if (n < 1) slideIndex = imgList.length;
+
+    for (i = 0; i < imgList.length; i++) {
+        imgList[i].style.display = "none";
+    }
+
+    imgList[slideIndex - 1].style.display = "block";
+}
+
+/*
+setInterval(() => {
+    plusDivs(1);
+}, 2000);
+1000 ms = 1 s
+*/
